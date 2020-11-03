1c9ce94bf7ced9ab13c2335ad68d576fe263f175
Nguyen Ba The Anh <rickyta1612@gmail.com>
2020-11-03 16:30:34 +0700

    topic_06

---
+++ selenium/webdriver/Topic_06_Textbox_TextArea.java
@@ -0,0 +1,65 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_06_Textbox_TextArea {
+	
+	//Global Variable
+	WebDriver driver;
+	String LocalFolder = System.getProperty("user.dir");
+	String email, userID, password, loginPageUrl;
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver", LocalFolder + "\\BrowserDrivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("http://demo.guru99.com/v4/");
+		email = "rickyta1612@gmail.com";
+		loginPageUrl = driver.getCurrentUrl();
+	}
+
+	@Test
+	public void TC_01_Register() {
+		driver.findElement(By.xpath("//a[text()='here']")).click();
+		driver.findElement(By.name("emailid")).sendKeys(email);
+		driver.findElement(By.name("btnLogin")).click();
+		
+		//Local Variable
+		userID = driver.findElement(By.xpath("//td[text()='User ID :']/following-sibling::td")).getText();
+		password = driver.findElement(By.xpath("//td[text()='Password :']/following-sibling::td")).getText();
+	}
+	
+	@Test
+	public void TC_02_Login() {
+		driver.get(loginPageUrl);
+		driver.findElement(By.name("uid")).sendKeys(userID);
+		driver.findElement(By.name("password")).sendKeys(password);
+		driver.findElement(By.name("btnLogin")).click();
+		Assert.assertEquals(driver.findElement(By.xpath("//marquee[@class='heading3']")).getText(), "Welcome To Manager's Page of Guru99 Bank");
+	}
+	
+	@Test
+	public void TC_03_New_Customer() {
+		
+	}
+	
+	@Test
+	public void TC_04_Edit_Customer() {
+		
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
